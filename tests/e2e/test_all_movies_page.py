27ce552f060311aee2cd9dee9fd1aa09bf90cbfc
Shawn Kiruba <eliezershawn@gmail.com>
2022-03-15 13:32:18 -0400

    Finishing tests for feature 1

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -9,6 +9,7 @@ def test_app():
 def test_all_movies_page(test_app):
     response = test_app.get('/movies')
     assert b'<h1 class="mb-5">All Movies</h1>' in response.data
+    assert b'<p class="mb-3">See our list of movie ratings below</p>' in response.data
     assert b'<th>Movie</th>' in response.data
     assert b'<th>Director</th>' in response.data
     assert b'<th>Rating</th>' in response.data
