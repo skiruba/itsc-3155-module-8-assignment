385c410a51f3db4a4a2a3213e36e72677cce0d7a
ChaosBlaze <chandrashekar9479@gmail.com>
2022-03-15 23:42:50 -0400

    Search Functionality

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,13 @@
 # TODO: Feature 3
+
+import pytest
+from app import app
+
+@pytest.fixture()
+def test_app():
+    return app.test_client()
+
+def test_search_movie_page(test_app):
+    response = test_app.get('/movies/search')
+    assert  b'<h1 class="mb-5">Search Movie Ratings</h1>' in response.data
+    assert b'<p class="mb-3">Search for a movie rating below</p>' in response.data
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,19 @@
 # TODO: Feature 3
+# TODO: Feature 3
+from src.repositories.movie_repository import movie_repository_singleton
+
+def test_search():
+    movie = movie_repository_singleton.create_movie('Batman Begins', 'Christopher Nolan', 4)
+    assert movie.title == 'Batman Begins'
+    assert movie.director == 'Christopher Nolan'
+    assert movie.rating == 4
+
+    movie = movie_repository_singleton.create_movie('The Dark Knight', 'Christopher Nolan', 1)
+    assert movie.title == 'The Dark Knight'
+    assert movie.director == 'Christopher Nolan'
+    assert movie.rating == 1
+
+    movie = movie_repository_singleton.create_movie('The Dark Knight Rises', 'Christopher Nolan', 2)
+    assert movie.title == 'The Dark Knight Rises'
+    assert movie.director == 'Christopher Nolan'
+    assert movie.rating == 2
